7e6ac20bede35870346368c3074512098a94eaec
Ahmed <markov916966@gmail.com>
2025-05-09 02:00:31 +0300

    hello world

--- public/index.php
+++ public/index.php
@@ -5,8 +5,8 @@
 require_once __DIR__ . '/paths.php';
 
 // URLs
-define('BASE_URL', '/SE/public'); // Adjust as needed
-define('APP_URL', 'http://localhost/rail-connect/public');
+define('BASE_URL', '/railway_sys/public'); // Adjust as needed
+define('APP_URL', 'http://localhost/railway_sys/public');
 
 // Start session
 session_start();
