1ace67062bc894eb16fd8f4c4ccba414eed21b31
Kerollos Emad <kerollos.em@gmail.com>
2025-05-09 02:16:13 +0300

    Update dashboard.php

--- views/user/dashboard.php
+++ views/user/dashboard.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') {
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="public/assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/index.css">
     <style>
         .dashboard-stat {
             border-radius: 10px;
